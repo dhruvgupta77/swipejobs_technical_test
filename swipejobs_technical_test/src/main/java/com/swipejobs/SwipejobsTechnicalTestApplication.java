8456bed036ec45296bff8d73052c4f8aaa4a0f78
Dhruv Gupta <dhruvgupta77@gmail.com>
2021-11-23 20:58:40 +0530

    Initial Commit

---
+++ swipejobs_technical_test/src/main/java/com/swipejobs/SwipejobsTechnicalTestApplication.java
@@ -0,0 +1,13 @@
+package com.swipejobs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwipejobsTechnicalTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwipejobsTechnicalTestApplication.class, args);
+	}
+
+}
